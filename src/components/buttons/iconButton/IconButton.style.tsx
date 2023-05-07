1120dd540d15253f275dd3c19678dec518b2d8f0
은배 <geb9211@gmail.com>
2023-05-07 23:49:05 +0900

    feat: add IconButton Component

---
+++ src/components/buttons/iconButton/IconButton.style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { IconButtonProps } from '@/components';
+
+export const Wrapper = styled.button<IconButtonProps>`
+  all: unset;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
---
+++ src/components/buttons/iconButton/IconButton.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Image from 'next/image';
+import { withThemeProvider } from '@/hoc/withThemeProvider';
+
+import * as S from './IconButton.style';
+
+export type IconButtonProps = {
+  iconName?: 'Left' | '';
+  width?: number;
+  height?: number;
+  onClick?: () => void;
+} & React.ComponentProps<'button'>;
+
+const IconButton = ({
+  iconName,
+  width,
+  height,
+  onClick,
+  ...props
+}: IconButtonProps) => {
+  return (
+    <S.Wrapper type="button" onClick={onClick} {...props}>
+      <Image
+        alt={`${iconName} 버튼`}
+        src={`/images/icons/${iconName}.svg`}
+        width={width}
+        height={height}
+      />
+    </S.Wrapper>
+  );
+};
+export default withThemeProvider(IconButton);
