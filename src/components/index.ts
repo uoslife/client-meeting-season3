caa1862add0f5c52810f6e34f7a7cef850082f4d
은배 <geb9211@gmail.com>
2023-05-07 23:51:28 +0900

    feat: components export index 작성

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+//buttons
+export { default as IconButton } from './buttons/iconButton/IconButton';
+export type { IconButtonProps } from './buttons/iconButton/IconButton';
+
+// typography
+export { default as Text } from './typography/text/Text';
+export type { TextProps } from './typography/text/Text';
+
+// header
+export { default as MainHeader } from './header/mainHeader/MainHeader';
