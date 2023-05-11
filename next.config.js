4853f43f43ac3d08b83c284d1b81d478d52dff3b
은배 <geb9211@gmail.com>
2023-05-11 16:07:18 +0900

    chore: change next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+};
 
 module.exports = nextConfig;
