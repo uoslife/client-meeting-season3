0124ab9f3987f95a9dd9292c12ff200ba0cc9036
은배 <geb9211@gmail.com>
2023-05-12 11:45:27 +0900

    chore: setting storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,32 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const path = require('path');
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-styling',
+    {
+      name: 'storybook-addon-next',
+      options: {
+        nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+  webpackFinal: async config => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+    return config;
+  },
+};
+export default config;
