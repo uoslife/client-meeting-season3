4d0847906c08c7b6ade88a99f9f0a05dbf8ffbb3
은배 <geb9211@gmail.com>
2023-05-16 03:17:29 +0900

    refactor: FooterStepButton 컴포넌트 폴더 변경

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ export { default as IconButton } from './buttons/iconButton/IconButton';
 export type { IconButtonProps } from './buttons/iconButton/IconButton';
 export { default as Checkbox } from './buttons/checkbox/Checkbox';
 export type { CheckboxProps } from './buttons/checkbox/Checkbox';
+export { default as FooterStepButton } from './buttons/footerStepButton/FooterStepButton';
 
 // typography
 export { default as Text } from './typography/text/Text';
@@ -12,6 +13,7 @@ export type { TextProps } from './typography/text/Text';
 
 // header
 export { default as MainHeader } from './header/mainHeader/MainHeader';
+export { default as ProgressHeader } from './header/progressHeader/ProgressHeader';
 
 // footer
 export { default as Footer } from './footer/Footer';
