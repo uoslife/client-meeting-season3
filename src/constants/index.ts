0235ab80ae09a5b2200f063ae62ffaf406deaaf1
은배 <geb9211@gmail.com>
2023-05-16 20:39:36 +0900

    feat: 미팅 종류에 따른 헤더 title constants 지정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,3 +6,19 @@ export const SOCIAL_LINK = {
 };
 
 export const PERSONAL_MAX_PAGE_ARR = [3, 6, 2, 3, 3];
+
+export const PERSONAL_PROGRESSBAR_TITLE = [
+  '01.내 정보 입력하기',
+  '02.나의 유형 알아보기',
+  '03.추가 정보 입력하기',
+  '04.선호 상대 정보 입력하기',
+  '05.선호 상대 유형 알아보기',
+];
+
+export const GROUP_PROGRESSBAR_TITLE = [
+  '01.팅 만들기',
+  '02.우리 팅 정보 입력하기',
+  '03.우리 팅 정보 입력하기',
+  '04.추가 정보 입력하기',
+  '05.만나고 싶은 팅 정보',
+];
