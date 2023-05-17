d58d745fd39df0993791979b6184b041e46f2a26
은배 <geb9211@gmail.com>
2023-05-17 21:29:35 +0900

    feat: applyData 목업 작성

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { ApplyDataArr } from '@/types/apply.type';
+
+export const mockApplyData: ApplyDataArr = [
+  { title_kr: '닉네임', title_en: 'nickname', data: '시대생' },
+  { title_kr: '성별', title_en: 'gender', data: '여자' },
+  { title_kr: '나이', title_en: 'gender', data: '23' },
+  { title_kr: '키', title_en: 'gender', data: '155' },
+  { title_kr: '학과', title_en: 'major', data: '산업디자인학과' },
+  { title_kr: '신분', title_en: 'shinbun', data: '재학생' },
+  { title_kr: '동물상', title_en: 'animal', data: '고양이' },
+  { title_kr: 'Q&A', title_en: 'Q_A', data: 'A1. 편한게 좋아 집 데이트하자' },
+  { title_kr: '관심사', title_en: 'interested', data: '여자' },
+  { title_kr: '흡연 여부', title_en: 'smoke', data: '안함' },
+];
+
+export default mockApplyData;
