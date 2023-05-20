ffaa6999905a579cdf5d5030c7dd97df7fa8df9a
youngchan Kim <aacz12035@gmail.com>
2023-05-20 12:56:18 +0900

    feat: DepartmentSelectBox 제작

---
+++ src/components/molecules/departmentSelectBox/DepartmentSelectBox.tsx
@@ -0,0 +1,113 @@
+'use client';
+
+import {
+  Col,
+  RoundedRectangleButton,
+  Row,
+  Text,
+  TextRoundInput,
+} from '@/components';
+import { useState, Dispatch, SetStateAction } from 'react';
+import useInput from '@/hooks/useInput';
+import { DEPARTMENTS } from '@/constants/departments';
+
+export type DepartmentSelectBoxProps = {
+  isGroup?: boolean;
+  selectedDepartments?: string[];
+  setSelectedDepartments?: Dispatch<SetStateAction<string[]>>;
+};
+
+const DepartmentSelectBox = ({
+  isGroup = false,
+  selectedDepartments,
+  setSelectedDepartments,
+}: DepartmentSelectBoxProps) => {
+  const [searchText, handleSearchText, setSearchText] = useInput('');
+  const [department, setDepartment] = useState<string[]>([]);
+
+  const updateDepartment = (isAdd: boolean, item?: string, i?: number) => {
+    return selectedDepartments!.filter((name, index) =>
+      isAdd ? name !== item : index !== i,
+    );
+    // 학과를 추가하냐 삭제하냐애 따른 fiter 메서드 분기 처리
+  };
+
+  const searchDepartment = (val: string) => {
+    const searchedResult = (text: string) =>
+      DEPARTMENTS.filter(({ name }) => name.includes(text));
+    // 검색 시, DEPARTMENTS 배열에서 searchText state에 맞는 정보 가져오기
+
+    setDepartment([...department, searchedResult(val)[0].name]);
+    setSearchText('');
+  };
+
+  const handleAddDepartment = (item: string) => () => {
+    if (isGroup && selectedDepartments?.length === 1) {
+      setSelectedDepartments!(updateDepartment(false, item));
+      setDepartment([]);
+      return;
+      // 학과 1개 선택 시,
+    }
+    setSelectedDepartments!([...selectedDepartments!, item]);
+    setDepartment([]);
+    // 학과 2개 이상 선택 시,
+  };
+
+  const handleDeleteDepartment = (item: string, i: number) => () => {
+    if (selectedDepartments?.includes(item)) {
+      setSelectedDepartments!(updateDepartment(true, item, i));
+      setDepartment([]);
+    }
+  };
+
+  return (
+    <Col gap={24}>
+      <Col gap={16}>
+        <TextRoundInput
+          placeholder={'원하시는 과를 입력해주세요.'}
+          isSearch={true}
+          value={searchText}
+          onChange={handleSearchText}
+          onClick={() => searchDepartment(searchText)}
+        />
+        {!!department[0] && (
+          <RoundedRectangleButton
+            fontSize={'sm'}
+            type={'skyBlue'}
+            label={department[0]}
+            height={32}
+            onClick={handleAddDepartment(department[0])}
+          />
+        )}
+      </Col>
+      <Col gap={14}>
+        <Row gap={4}>
+          <Text label={'선택된 학과'} weight={500} color={'#3B4046'} />
+          <Text
+            label={`(${selectedDepartments?.length.toString()}개)`}
+            weight={500}
+            color={'#80AAFF'}
+          />
+        </Row>
+        <Row gap={8} style={{ flexWrap: 'wrap' }}>
+          {!!selectedDepartments?.length &&
+            selectedDepartments.map((name, i) => {
+              return (
+                <RoundedRectangleButton
+                  fontSize={'sm'}
+                  type={'primary'}
+                  label={name}
+                  height={32}
+                  isDelete={true}
+                  onClick={handleDeleteDepartment(name, i)}
+                  key={i}
+                />
+              );
+            })}
+        </Row>
+      </Col>
+    </Col>
+  );
+};
+
+export default DepartmentSelectBox;
