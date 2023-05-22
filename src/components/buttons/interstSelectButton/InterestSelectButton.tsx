d70126bc29ad1f23177e1d52190e451f0206bc9d
youngchan Kim <aacz12035@gmail.com>
2023-05-22 19:54:42 +0900

    feat: InterestSelectButton 공통 컴포넌트 추가

---
+++ src/components/buttons/interstSelectButton/InterestSelectButton.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import * as S from './InterestSelectButton.style';
+import { Text } from '@/components';
+import { Combine } from '@/types/utils.type';
+import Image from 'next/image';
+
+export type InterestSelectButtonProps = Combine<
+  {
+    isActive: boolean;
+    order: number;
+    label: string;
+    onClick?: () => void;
+  },
+  React.ComponentProps<'button'>
+>;
+
+const InterestSelectButton = ({
+  isActive,
+  order,
+  label,
+  onClick,
+  ...props
+}: InterestSelectButtonProps) => {
+  return (
+    <S.Wrapper {...props} onClick={onClick}>
+      <S.ImgWrapper isActive={isActive}>
+        <Image
+          src={`/images/interests/${order}.svg`}
+          alt={''}
+          width={54}
+          height={54}
+        />
+      </S.ImgWrapper>
+      <Text label={label} weight={600} color={'#808A98'} />
+    </S.Wrapper>
+  );
+};
+export default InterestSelectButton;
