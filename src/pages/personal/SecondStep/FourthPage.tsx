7906f94dea0f632706c3337931c4a76b33bf9ace
Chaehee03 <sch003322@gmail.com>
2023-05-22 10:53:52 +0900

    personal secondStep 페이지 제작 완료

--- src/pages/personal/SecondStep/FourthPage.tsx
+++ src/pages/personal/SecondStep/FourthPage.tsx
@@ -1,7 +1,54 @@
-import { StepProps } from '@/types/step.type';
+'use client';
 
-const FourthPage = ({ setIsFinishPage }: StepProps) => {
-  return <></>;
-};
+import { Button, Col, Text } from '@/components';
+import { StepProps } from '@/types/step.type';
+import Image from 'next/image';
+import Image4 from 'public/images/personal/my-types/4.svg';
+
+function FourthPage({ setIsFinishPage }: StepProps) {
+  return (
+    <Col fill align="center">
+      <Col gap={32} align="center" padding="32px 24px 32px 24px">
+        <Col gap={8} align="center">
+          <Text
+            as="p"
+            color="#4C89FF"
+            font="LeferiPoint-SpecialA"
+            label="Q1."
+            size="xl"
+            weight={700}
+          />
+
+          <Text
+            as="p"
+            color="#3B4046"
+            font="LeferiBaseType-RegularA"
+            label="연인과의 만남에 있어..."
+            size="lg"
+            weight={700}
+          />
+        </Col>
+
+        <Image src={Image4} alt="" width={300} height={240} />
+        <Col gap={12}>
+          <Button
+            height={56}
+            label="혼자만의 휴식 시간도 필요해요"
+            textSize="sm"
+            primary="inactive"
+            width={366}
+          />
+          <Button
+            height={56}
+            label="시간 날 때마다 함께 있고 싶어요"
+            textSize="sm"
+            primary="inactive"
+            width={366}
+          />
+        </Col>
+      </Col>
+    </Col>
+  );
+}
 
 export default FourthPage;
