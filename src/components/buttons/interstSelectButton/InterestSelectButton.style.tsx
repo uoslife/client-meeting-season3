6ae994e6bc2aa792c793d5fe07bac08bb54b5bc0
youngchan Kim <aacz12035@gmail.com>
2023-05-22 19:54:42 +0900

    feat: InterestSelectButton 공통 컴포넌트 추가

---
+++ src/components/buttons/interstSelectButton/InterestSelectButton.style.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { InterestSelectButtonProps } from '@/components/buttons/interstSelectButton/InterestSelectButton';
+
+export const Wrapper = styled.button<InterestSelectButtonProps>`
+  all: unset;
+  box-sizing: border-box;
+  position: relative;
+  width: 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const ImgWrapper = styled.div<InterestSelectButtonProps>`
+  width: 90px;
+  height: 90px;
+  display: flex;
+  border-radius: 50px;
+  align-items: center;
+  justify-content: center;
+  background: ${({ isActive }) => (isActive ? '#000000B2' : '#F0F5FF')};
+`;
+
+export const GridWrapper = styled.div<InterestSelectButtonProps>`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-column-gap: 0;
+  grid-row-gap: 16px;
+  width: 100%;
+  cursor: pointer;
+`;
