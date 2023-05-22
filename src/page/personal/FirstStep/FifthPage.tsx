06931060f886181c6136144da09f60c7599f7eed
youngchan Kim <aacz12035@gmail.com>
2023-05-22 19:56:02 +0900

    feat: 1대1 step1 page5 관심사 페이지 제작

--- src/page/personal/FirstStep/FifthPage.tsx
+++ src/page/personal/FirstStep/FifthPage.tsx
@@ -1,7 +1,50 @@
 import { StepProps } from '@/types/step.type';
+import { Col, InterestSelectButton, Text } from '@/components';
+import * as S from '@/components/buttons/interstSelectButton/InterestSelectButton.style';
+import { INTERESTS } from '@/constants';
+import { useEffect, useState } from 'react';
 
 const FifthPage = ({ setIsFinishPage }: StepProps) => {
-  return <></>;
+  const [interestValue, setInterestValue] = useState<string[]>([]);
+  const handleSelectInterest = (value: string) => () => {
+    const deleteInterestState = interestValue.filter(item => item != value);
+
+    if (interestValue.length < 3 && !interestValue.includes(value))
+      return setInterestValue([...interestValue, value]);
+    setInterestValue(deleteInterestState);
+  };
+  const handleIsFinishPage = () => {
+    if (interestValue.length === 3) setIsFinishPage(true);
+  };
+
+  useEffect(() => {
+    handleIsFinishPage();
+  }, [interestValue]);
+
+  return (
+    <Col gap={32} padding={'32px 24px'}>
+      <Col align={'center'}>
+        <Text
+          label={'11. 본인의 관심사를 3가지 선택해주세요.'}
+          color={'#3B4046'}
+          weight={700}
+        />
+      </Col>
+      <S.GridWrapper>
+        {INTERESTS.map((item, i) => {
+          return (
+            <InterestSelectButton
+              key={i}
+              isActive={interestValue.includes(item)}
+              order={i + 1}
+              label={INTERESTS[i]}
+              onClick={handleSelectInterest(item)}
+            />
+          );
+        })}
+      </S.GridWrapper>
+    </Col>
+  );
 };
 
 export default FifthPage;
