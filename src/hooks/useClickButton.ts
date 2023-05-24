21b1e24f9f76cee33819b3d2e2f91b2092fbfd52
은배 <geb9211@gmail.com>
2023-05-24 22:26:53 +0900

    feat: useClickButton 로직 변경

--- src/hooks/useClickButton.ts
+++ src/hooks/useClickButton.ts
@@ -3,30 +3,49 @@ import { useEffect, useState } from 'react';
 type ButtonState = {
   label: string;
   active: boolean;
+  order: number;
 };
 
 // nameArr = ['a', 'b', ...];
-const useClickButton = (nameArr: Array<string>) => {
+
+const useClickButton = (
+  nameArr: Array<string>,
+  maxSelectedCount: number,
+): [
+  (order: number) => void,
+  (order: number) => boolean,
+  boolean,
+  ButtonState[],
+] => {
   const initState: Array<ButtonState> = [];
-  nameArr.forEach(label => initState.push({ label, active: false }));
+  nameArr.map((label, i) => initState.push({ label, active: false, order: i }));
   const [state, setState] = useState(initState);
 
   const onClickButton = (order: number) => {
-    setState(() => {
-      const newState = initState;
-      newState[order].active = true;
-      return newState;
-    });
+    const isMaxSelectedCount =
+      state.filter(data => data.active).length >= maxSelectedCount;
+    setState(prev =>
+      prev.map(item =>
+        item.order === order
+          ? isMaxSelectedCount
+            ? item.active
+              ? { ...item, active: !item.active }
+              : item
+            : { ...item, active: !item.active }
+          : item,
+      ),
+    );
   };
 
   const [isClickedButton, setIsClickedButton] = useState(false);
-  const [selectedLabel, setSelectedLabel] = useState('');
+  const [selectedLabel, setSelectedLabel] = useState<ButtonState[]>([]);
 
   useEffect(() => {
-    const activatedObj = state.find(data => data.active);
-    if (!!activatedObj) {
+    const activatedObj = state.filter(data => data.active);
+
+    if (activatedObj.length !== 0) {
       setIsClickedButton(true);
-      setSelectedLabel(activatedObj.label);
+      setSelectedLabel(activatedObj);
     }
   }, [state]);
 
@@ -34,7 +53,7 @@ const useClickButton = (nameArr: Array<string>) => {
     return state[order].active;
   };
 
-  return { onClickButton, buttonActiveState, isClickedButton, selectedLabel };
+  return [onClickButton, buttonActiveState, isClickedButton, selectedLabel];
 };
 
 export default useClickButton;
