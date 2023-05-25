643cefb6507e01745db20270b7d0af4cde28e7bc
은배 <geb9211@gmail.com>
2023-05-25 19:00:53 +0900

    feat: redux-persist에 맞게 provider 작성

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,60 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import applyInfoReducer from './feature/applyInfo';
 
-const rootReducer = {
-  applyInfo: applyInfoReducer,
+import applyInfoReducer from './feature/applyInfo';
+import commonReducer from './feature/common/commonReducer';
+import personalReducer from './feature/meetingType/personalReducer';
+import groupReducer from './feature/meetingType/groupReducer';
+
+// import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key: any) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: any, value: any) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: any) {
+      return Promise.resolve();
+    },
+  };
 };
 
+const storage =
+  typeof window === 'undefined'
+    ? createNoopStorage()
+    : createWebStorage('local');
+
+const rootReducer = combineReducers({
+  applyInfo: applyInfoReducer,
+  common: commonReducer,
+  personal: personalReducer,
+  group: groupReducer,
+});
+
+// create slice
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['applyInfo', 'common', 'personal', 'group'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
