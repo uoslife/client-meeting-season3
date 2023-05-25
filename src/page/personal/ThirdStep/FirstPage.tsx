ecff0f9448fd31cc52567c945c551b7643d364fa
은배 <geb9211@gmail.com>
2023-05-25 21:13:29 +0900

    feat: 1대1 소개팅 ThirdStep FirstPage 작성

--- src/page/personal/ThirdStep/FirstPage.tsx
+++ src/page/personal/ThirdStep/FirstPage.tsx
@@ -1,7 +1,120 @@
+import { Text, Col, Button, Slider } from '@/components';
+import { AGE_SLIDER_ARR, HEIGHT_SLIDER_ARR } from '@/constants';
+import useClickButton from '@/hooks/useClickButton';
+import {
+  setPreferAgePersonal,
+  setPreferHeight,
+  setPreferStudentType,
+} from '@/store/feature/meetingType/personalReducer';
+import { useAppDispatch } from '@/store/store';
 import { StepProps } from '@/types/step.type';
+import { useEffect, useState } from 'react';
 
 const FirstPage = ({ setIsFinishPage }: StepProps) => {
-  return <></>;
+  const dispatch = useAppDispatch();
+  const [age, setAge] = useState({ min: 0, max: 100 });
+  const [height, setHeight] = useState({ min: 0, max: 80 });
+
+  const studentTypeArr = ['학부생', '대학원생', '졸업생'];
+  const [
+    onClickStudentTypeButton,
+    studentTypeButtonActiveState,
+    isClickedStudentType,
+    studentType,
+  ] = useClickButton(studentTypeArr, 1);
+
+  useEffect(() => {
+    const ageArr = [AGE_SLIDER_ARR[age.min / 10], AGE_SLIDER_ARR[age.max / 10]];
+    const heightArr = [
+      HEIGHT_SLIDER_ARR[height.min / 10],
+      HEIGHT_SLIDER_ARR[height.max / 10],
+    ];
+    dispatch(setPreferAgePersonal(ageArr));
+    dispatch(setPreferHeight(heightArr));
+    if (isClickedStudentType)
+      dispatch(setPreferStudentType(studentType[0].label));
+
+    if (isClickedStudentType) setIsFinishPage(true);
+    else setIsFinishPage(false);
+  }, [
+    age.max,
+    age.min,
+    dispatch,
+    height.max,
+    height.min,
+    isClickedStudentType,
+    setIsFinishPage,
+    studentType,
+  ]);
+
+  return (
+    <>
+      <Col gap={56} padding="32px 24px 120px" align="center">
+        <Col gap={32} align="center">
+          <Text
+            label={'1. 선호하는 상대의 나이를 선택해주세요.'}
+            weight={700}
+            font="LeferiBaseType-RegularA"
+            color="#3B4046"
+          />
+          <Slider
+            guideText={AGE_SLIDER_ARR}
+            labelExplain="세"
+            min={0}
+            max={100}
+            step={10}
+            value={age}
+            onChange={setAge}
+          />
+        </Col>
+        <Col gap={32} align="center">
+          <Text
+            label={'2. 선호하는 상대의 키를 선택해주세요.'}
+            weight={700}
+            font="LeferiBaseType-RegularA"
+            color="#3B4046"
+          />
+          <Slider
+            guideText={HEIGHT_SLIDER_ARR}
+            labelExplain=" (cm)"
+            min={0}
+            max={80}
+            step={10}
+            value={height}
+            onChange={setHeight}
+          />
+        </Col>
+        <Col gap={32}>
+          <Col gap={12} align="center">
+            <Text
+              label={'3. 선호하는 상대의 신분을 선택해주세요.'}
+              weight={700}
+              font="LeferiBaseType-RegularA"
+              color="#3B4046"
+            />
+            <Text
+              label="복수 선택이 가능하며 무관한 경우 모두 선택해주세요."
+              size="sm"
+              color="#656D78"
+            />
+          </Col>
+          <Col gap={12}>
+            {studentTypeArr.map((label, i) => (
+              <Button
+                key={i}
+                label={label}
+                primary={
+                  studentTypeButtonActiveState(i) ? 'active' : 'inactive'
+                }
+                textSize="base"
+                onClick={() => onClickStudentTypeButton(i)}
+              />
+            ))}
+          </Col>
+        </Col>
+      </Col>
+    </>
+  );
 };
 
 export default FirstPage;
