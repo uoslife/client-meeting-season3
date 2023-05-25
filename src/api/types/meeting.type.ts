7f02cfea0c622a92b261691963ce632ad8c94955
은배 <geb9211@gmail.com>
2023-05-25 23:59:30 +0900

    feat: meeting API type 작성

---
+++ src/api/types/meeting.type.ts
@@ -0,0 +1,36 @@
+/** Params Types */
+export type TeamType = 'SINGLE' | 'TRIPLE';
+export type TeamTypeWithObject = { teamType: TeamType };
+
+export type IsTeamLeader = boolean;
+export type IsTeamLeaderWithObject = { isTeamLeader: IsTeamLeader };
+
+export type Code = string;
+export type CodeWithObject = { code: Code };
+
+/** */
+
+export type CreateTeamParams = TeamTypeWithObject & IsTeamLeaderWithObject;
+export type CreateTeamResponse = {};
+
+export type PostTeamInfoParams = TeamTypeWithObject & IsTeamLeaderWithObject;
+export type PostTeamInfoRequest = {
+  informationDistance: 'string';
+  informationFilter: 'string';
+  informationMeetingTime: 'string';
+  preferenceDistance: 'string';
+  preferenceFilter: 'string';
+};
+export type PostTeamInfoResponse = {};
+
+export type GetTeamInfoParams = TeamTypeWithObject;
+export type GetTeamInfoResponse = {};
+
+export type DeleteTeamParams = TeamTypeWithObject & IsTeamLeaderWithObject;
+export type DeleteTeamResponse = {};
+
+export type GetTeamStatusParams = TeamTypeWithObject;
+export type GetTeamStatusResponse = {};
+
+export type EnterTeamParams = TeamTypeWithObject & CodeWithObject;
+export type EnterTeamResponse = {};
