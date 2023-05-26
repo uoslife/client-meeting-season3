e6a55fffd47ef560dcb957549e2d552aff472165
은배 <geb9211@gmail.com>
2023-05-26 10:19:00 +0900

    feat: layout 단에 registerDevice 함수 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,53 @@
-import StyledComponentsRegistry from '@/lib/registry';
+'use client';
+
+import { authAPI } from '@/api';
 import { Providers } from '@/store/provider';
+import { NativeActions } from '@/utils/native/actions';
+import { NativeBridge } from '@/utils/native/bridge';
+import { useState, useEffect } from 'react';
+import { UserIdContext } from '@/utils/context/userIdContext';
+
+import StyledComponentsRegistry from '@/lib/registry';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const [topMargin, setTopMargin] = useState<number>(0);
+  const [userId, setUserId] = useState<number>(0);
+
+  const loginWithDevice = async (nativeActions: NativeActions) => {
+    const [device_id, secret] = await Promise.all([
+      nativeActions.getDeviceId(),
+      nativeActions.getDeviceSecret(),
+    ]);
+
+    if (!device_id || !secret) {
+      // web, production
+      if (process.env.NODE_ENV === 'production') return;
+      // web, development
+      else if (process.env.NODE_ENV === 'development') {
+        return authAPI.registerDevice({
+          device_id: process.env.NEXT_PUBLIC_DEVICE_ID as string,
+          secret: process.env.NEXT_PUBLIC_DEVICE_SECRET as string,
+        });
+      }
+    } else {
+      /** mobile, production */
+      return authAPI.registerDevice({
+        device_id,
+        secret,
+      });
+    }
+  };
+  const handleTopMargin = async (nativeActions: NativeActions) => {
+    const topMargin = await nativeActions.getSafeArea();
+    return topMargin || 0;
+  };
+
+  useEffect(() => {
+    const nativeActions = new NativeActions(new NativeBridge());
+    loginWithDevice(nativeActions).then();
+    handleTopMargin(nativeActions).then(margin => setTopMargin(margin));
+  }, []);
+
   return (
     <html lang="ko">
       <head>
@@ -17,10 +63,12 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-dynamic-subset.css"
         />
       </head>
-      <body>
-        <StyledComponentsRegistry>
-          <Providers>{children}</Providers>
-        </StyledComponentsRegistry>
+      <body style={{ paddingTop: `${topMargin}px` }}>
+        <UserIdContext.Provider value={{ userId, setUserId }}>
+          <StyledComponentsRegistry>
+            <Providers>{children}</Providers>
+          </StyledComponentsRegistry>
+        </UserIdContext.Provider>
       </body>
     </html>
   );
