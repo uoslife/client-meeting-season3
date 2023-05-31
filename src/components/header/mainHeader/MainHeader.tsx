39212d920fb8055364a609e9eb5ac65f24f445f4
은배 <geb9211@gmail.com>
2023-06-01 01:41:36 +0900

    feat: 메인페이지 헤더 뒤로가기 버튼 함수 클릭 범위 확장

--- src/components/header/mainHeader/MainHeader.tsx
+++ src/components/header/mainHeader/MainHeader.tsx
@@ -10,20 +10,25 @@ const MainHeader = () => {
   };
   return (
     <S.Header>
-      <IconButton
-        iconName="Left"
-        width={12}
-        height={20}
+      <div
         onClick={onClickBackButton}
-      />
-      <Text
-        label="시대생 메인"
-        as="div"
-        size="base"
-        weight={600}
-        color={'#2E74FF'}
-        style={{ marginLeft: '8px', paddingTop: '2px' }}
-      />
+        style={{ display: 'flex', cursor: 'pointer' }}
+      >
+        <IconButton
+          iconName="Left"
+          width={12}
+          height={20}
+          // onClick={onClickBackButton}
+        />
+        <Text
+          label="시대생 메인"
+          as="div"
+          size="base"
+          weight={600}
+          color={'#2E74FF'}
+          style={{ marginLeft: '8px', paddingTop: '2px' }}
+        />
+      </div>
     </S.Header>
   );
 };
