0cac5b7ced31d9c8022441a95c0b68c5e6693331
은배 <geb9211@gmail.com>
2023-07-02 02:01:22 +0900

    feat: Button 컴포넌트 테스트 작성

--- src/components/buttons/button/Button.cy.tsx
+++
@@ -1,8 +0,0 @@
-import Button from './Button';
-
-it('uses custom text for the button label', () => {
-  cy.mount(
-    <Button primary={'disabled'} textSize={'2xl'} label={'Click me!'} />,
-  );
-  cy.get('button').should('contains.text', 'Click me!');
-});
